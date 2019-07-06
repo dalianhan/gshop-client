b49ab2c3d091635e76bb0192b531d07a9f41b6ed
Your Name <email@wer123.com>
2019-07-06 16:31:38 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FooterGuide/>
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+  export default {
+    name:"app",
+    data(){
+      return{
+        msg:'App Component'
+      }
+    },
+    components:{
+      FooterGuide
+    }
+
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
+
